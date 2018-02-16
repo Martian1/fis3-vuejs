e0e873ccdcae4fb6877c76f0e4532214fa5929b2
Martian1 <Martian1@users.noreply.github.com>
2018-02-16 16:45:20 +0800

    Create home.vue

---
+++ src/page/home/home.vue
@@ -0,0 +1,28 @@
+<template>
+<div class="home-content">
+    <h1>{{name}}</h1>
+    <div class="icon icon1"></div><div class="icon icon2"></div><div class="icon icon3"></div>
+    <img src="static/images/img.jpg" />
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'HOME'
+        }
+    },
+    created() {
+        console.log('home page');
+    },
+    methods: {
+    }
+};
+</script>
+
+<style lang="less">
+.home-content {
+    color: #000000;
+}
+</style>
