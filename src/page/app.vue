b1855e9947eac95208058a52b506fb8c64bceda7
Martian1 <Martian1@users.noreply.github.com>
2018-02-16 16:44:58 +0800

    Create app.vue

---
+++ src/page/app.vue
@@ -0,0 +1,7 @@
+<template>
+    <div id="app">
+        <router-view class="view" name="header"></router-view>
+        <router-view class="view"></router-view>
+        <router-view class="view" name="footer"></router-view>
+    </div>
+</template>
